841c034c5b05232b3449bc2620104058f06fa904
James Brumond <james@jbrumond.me>
2017-05-09 21:39:32 -0700

    add meta property to HttpError

--- src/http-error.js
+++ src/http-error.js
@@ -2,10 +2,11 @@
 import { STATUS_CODES } from 'http';
 
 export class HttpError {
-	constructor(status, message) {
+	constructor(status, message, meta) {
 		this.statusCode = status;
 		this.statusMessage = STATUS_CODES[status];
 		this.message = message;
+		this.meta = meta;
 	}
 
 	toString() {
